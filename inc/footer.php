e1e10ca2d32b77ccfb300730693184c2660e4fba
Syed Zaman Mostafiz <zamanwebdeveloper>
2019-09-23 00:12:56 +0600

    07.CRUDwithPHPOOPandMySQLi Database Insert in this Project

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+ </section>
+<section class="footeroption">
+	<p>&copy; <?php echo date("Y"); ?> Training with live project</p>
+		<h2><a href="https://zamanwebdeveloper.com/" target="_blank"><?php echo "zamanwebdeveloper.com/"; ?></a></h2>
+	</section>
+</div>
+</body>
+</html>
